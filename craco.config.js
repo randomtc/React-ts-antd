23c5291b015ec8e69fa8d71b6c238fc15631f7bf
Geshihao <2714780821@qq.com>
2022-09-13 13:22:08 +0800

    1

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,10 @@
-const CracoLessPlugin = require('craco-less')
-const path = require("path")//导入path模块
 module.exports = {
 
   webpack: {
     // 配置别名
     alias: {
       // 约定：使用 @ 表示 src 文件所在路径
-      "@": path.resolve(__dirname, 'src')
+      "@": require("path").resolve(__dirname, 'src')
     },
 
   },
@@ -14,7 +12,7 @@ module.exports = {
   //使用less
   plugins: [
     {
-      plugin: CracoLessPlugin,
+      plugin:  require('craco-less'),
       options: {
         lessLoaderOptions: {
           lessOptions: {
